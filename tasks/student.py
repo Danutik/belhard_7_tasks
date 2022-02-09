2bd94e3e80969983b41db5dd636130259b6bbb74
Danuta Prakaptsova <danutik@mail.ru>
2022-02-09 22:13:35 +0300

    ready 1,2,4

--- tasks/student.py
+++ tasks/student.py
@@ -19,3 +19,32 @@
 
 Вывести студентов, у которых средний балл больше 5
 """
+
+
+class Student:
+    surname: str
+    name: str
+    group: int
+    average_score: int
+
+    def __init__(self, surname, name, group, average_score):
+        self.surname = surname
+        self.name = name
+        self.group = group
+        self.average_score = average_score
+
+    def __eq__(self, other):
+        return self.average_score == other.average_score
+
+    def __lt__(self, other):
+        return self.average_score < other.average_score
+
+    def __le__(self, other):
+        return self.average_score <= other.average_score
+
+    def max_score(self):
+        if self.average_score > 5:
+            return self
+
+    def __repr__(self):
+        return f"surname: {self.surname}, name: {self.name}, group: {self.group} average_score: {self.average_score}"
